cb6b00e559d7acaa094f0f9b90571a2eff9eb259
tutkuince <tutku.ince@outlook.com>
2020-03-11 12:23:41 +0300

    Admin User

--- src/main/java/com/muditasoft/springsecurity/configuration/AppSecurityConfig.java
+++ src/main/java/com/muditasoft/springsecurity/configuration/AppSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.muditasoft.springsecurity.configuration;
 
+import com.muditasoft.springsecurity.security.ApplicationUserRole;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.muditasoft.springsecurity.security.ApplicationUserRole.ADMIN;
+import static com.muditasoft.springsecurity.security.ApplicationUserRole.STUDENT;
 
 @Configuration
 @EnableWebSecurity
@@ -40,13 +44,13 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails johnSmithUser = User.builder()
                 .username("JohnSmith")
                 .password(passwordEncoder.encode("password"))
-                .roles("STUDENT")   // ROLE_STUDENT
+                .roles(STUDENT.name())   // ROLE_STUDENT
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles("ADMIN")
+                .roles(ADMIN.name())
                 .build();
 
         return new InMemoryUserDetailsManager(johnSmithUser, lindaUser);
---
+++ src/main/java/com/muditasoft/springsecurity/security/ApplicationUserRole.java
@@ -0,0 +1,22 @@
+package com.muditasoft.springsecurity.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.muditasoft.springsecurity.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+}
