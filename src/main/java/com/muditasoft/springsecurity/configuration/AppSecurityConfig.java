56c6192e618919e879213b9813f93fe1a8d5c0d3
tutkuince <tutku.ince@outlook.com>
2020-03-11 11:53:24 +0300

    In memory user details manager

--- src/main/java/com/muditasoft/springsecurity/configuration/AppSecurityConfig.java
+++ src/main/java/com/muditasoft/springsecurity/configuration/AppSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.muditasoft.springsecurity.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +25,17 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
 
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails johnSmithUser = User.builder()
+                .username("JohnSmith")
+                .password("password")
+                .roles("STUDENT")   // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(johnSmithUser);
     }
 }
